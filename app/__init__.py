b75dac436236ff9bd1e666fd91c8ad3da3abcd1f
Ihsan <muhamad.ihsann12@gmail.com>
2026-08-15 19:54:23 +0700

    feat: configure SQLAlchemy connection to revoshop_db

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("app.config.Config")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.routes.products import products_bp
+    from app.routes.users import users_bp
+    app.register_blueprint(products_bp)
+    app.register_blueprint(users_bp)
+
+    return app
